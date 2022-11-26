cc3261505be4375967307e84d2d3f28aa25c987c
Chris <cjhelms1428@gmail.com>
2022-11-26 11:49:42 -0600

    Fix purple float windows

--- init.lua
+++ init.lua
@@ -45,6 +45,11 @@ require('packer').startup(function(use)
 end)
 
 -- settings
+vim.api.nvim_set_hl(0, 'FloatBorder', {bg='#3B4252', fg='#5E81AC'})
+vim.api.nvim_set_hl(0, 'NormalFloat', {bg='#3B4252'})
+vim.api.nvim_set_hl(0, 'TelescopeNormal', {bg='#3B4252'})
+vim.api.nvim_set_hl(0, 'TelescopeBorder', {bg='#3B4252'})
+
 vim.g.mapleader = ' '
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
